881fc498228958d64e32541a63422b34588f6f17
Everton Agilar <evertonagilar@gmail.com>
2018-05-29 13:23:13 -0300

    improvements

--- erlangms/build.sh
+++ erlangms/build.sh
@@ -35,10 +35,12 @@ if [ ! -z "$ID_IMAGE" ]; then
 	sudo docker rmi -f $ID_IMAGE > /dev/null 2>&1
 fi
 
-#sudo docker build --no-cache -t $IMAGE:$VERSION $(dirname $0)
-sudo docker build -t $IMAGE:$VERSION $(dirname $0)
+sudo docker build --no-cache -t $IMAGE:$VERSION $(dirname $0)
+#sudo docker build -t $IMAGE:$VERSION $(dirname $0)
 
 ID=$(sudo docker images | grep "$IMAGE" | head -n 1 | awk '{print $3}')
 
 sudo docker tag "$ID" $IMAGE:latest
 sudo docker tag "$ID" $IMAGE:$VERSION
+sudo docker tag "$ID" erlangms/erlangms
+sudo docker tag "$ID" docker.io/erlangms/erlangms
