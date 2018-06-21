bd7cab188f3d824ab3a1b07cac059967dde1fdc1
Everton Agilar <evertonagilar@gmail.com>
2018-06-21 12:20:35 -0300

    improve erlangms_rest_server

---
+++ erlangms/compose/erlangms_rest_server/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# author Everton de Vargas Agilar <<evertonagilar@gmail.com>>
+#
+
+clear
+sudo docker swarm leave --force > /dev/null 2>&1
+sudo docker-compose down > /dev/null 2>&1
+sudo docker-compose build --force-rm --pull
+sudo docker-compose up --force-recreate --remove-orphans
